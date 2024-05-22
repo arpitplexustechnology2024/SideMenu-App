75bdfb7a318c968b69871cd01579786be699e325
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-22 12:15:48 -0700

    Initial Commit

---
+++ SideMenu App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenu App
+//
+//  Created by Arpit iOS Dev. on 22/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
