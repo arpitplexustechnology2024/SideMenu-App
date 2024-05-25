55145f084a5dfeef6c51ae379e26c3dba4bab9c4
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-25 12:48:07 -0700

    Commit

---
+++ SideMenu App/SideMenuableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SideMenuableViewCell.swift
+//  SideMenu App
+//
+//  Created by Arpit iOS Dev. on 22/05/24.
+//
+
+import UIKit
+
+class SideMenuableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
